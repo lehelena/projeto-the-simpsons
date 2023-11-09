5fa67ad07e6d64c5d1f8adbaa103d57901e9f6cb
Letícia Helena <leticia1helena1997@gmail.com>
2023-11-09 16:03:21 -0300

    projetosimpsons

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+/* 
+
+    OBJETIVO 1 - quando clicar no botão do personagem na lista, marcar o botao como selecionado 
+        
+	OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem
+       
+*/
+
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+	botao.addEventListener("click", () => {
+		desselecionarBotao();
+		desselecionarPersonagem();
+
+		botao.classList.add("selecionado");
+		personagens[indice].classList.add("selecionado");
+	});
+});
+
+function desselecionarPersonagem() {
+	const personagemSelecionado = document.querySelector(".personagem.selecionado");
+	personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+	const botaoSelecionado = document.querySelector(".botao.selecionado");
+	botaoSelecionado.classList.remove("selecionado");
+}
